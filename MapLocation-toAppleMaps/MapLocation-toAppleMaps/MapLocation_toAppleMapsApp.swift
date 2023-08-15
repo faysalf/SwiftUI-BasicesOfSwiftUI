0de962d7b6e81cd784214912fb39d541b8843667
Md. Faysal Ahmed <mdfaysalahmedf@gmail.com>
2023-08-16 01:16:10 +0600

    Init project

---
+++ MapLocation-toAppleMaps/MapLocation-toAppleMaps/MapLocation_toAppleMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLocation_toAppleMapsApp.swift
+//  MapLocation-toAppleMaps
+//
+//  Created by Md. Faysal Ahmed on 16/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MapLocation_toAppleMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
